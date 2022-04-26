a059894921b5927de02cb41f2990f8aff15d2358
Benjamin Wu <benjaminwu17@gmail.com>
2022-04-25 20:34:25 -0700

    Updated enhance

--- enhance.py
+++ enhance.py
@@ -8,8 +8,20 @@ headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
 headers["User-Agent"] = "BlackDesert"
 
+wep_bs = 16001
+arm_bs = 16002
+wep_conc = 16004
+arm_conc = 16005
+memes = 44195
+
 
 def getPrice(itemID, enhanceLevel):
+    """Returns the latest price of an item with the given itemID and enhanceLevel
+
+    Args:
+        itemID (int): ID of the item (can be found on BDOCodex)
+        enhanceLevel (int): Enhance level of the item (0-20, 0 being the base item, 16-20 for PRI-PEN)
+    """    
     data = f"""
     {{
     "keyType": 0,
@@ -23,7 +35,52 @@ def getPrice(itemID, enhanceLevel):
     priceData = parsed_json['resultMsg']
     x = priceData.rfind("-")
     latestPrice = priceData[x + 1:len(priceData)]
-    return(latestPrice)
+    return(int(latestPrice))
 
+
+def calculateEnhanceCost(itemID, initialEnhance, finalEnhance):
+    """Returns the cost of enhancing an item from initialEnhance to finalEnhance
+
+    Args:
+        itemID (int): ID of the item (can be found on BDOCodex)
+        initialEnhance (int): Initial enhance level of the item (0-20, 0 being the base item, 16-20 for PRI-PEN)
+        finalEnhance (int): Final enhance level of the item (0-20, 0 being the base item, 16-20 for PRI-PEN)
+    """    
+    price = getPrice(itemID, initialEnhance) # Price of item at the initialEnhance level
+    return(price)
 
 print(getPrice(732313, 20))
+
+
+
+
+
+"""
+If Armor or Weapon Then
+    If R < 70% Then BR * (1 + (0.1 x FS)) Else
+        If R >= 70% And R < 90% Then RT + (BR * (1 + (.02 * (FS - FST)))) Else
+            If R >= 90% Then 90%
+
+If Accessory Then
+    If PRI Then
+        If R < 70% Then BR * (1 + (0.1 x FS)) Else
+            If R >= 70% And R < 90% Then RT + (BR * (1 + (.02 * (FS - FST)))) Else
+                If R >= 90% Then 90%
+    If DUO Then
+        If R < 50% Then BR * (1 + (0.1 x FS)) Else
+            If R >= 50% And R < 90% Then RT + (BR * (1 + (.02 * (FS - FST)))) Else
+                If R >= 90% Then 90%
+    If TRI Then
+        If R < 40% Then BR * (1 + (0.1 x FS)) Else
+            If R >= 40% And R < 90% Then RT + (BR * (1 + (.02 * (FS - FST)))) Else
+                If R >= 90% Then 90%
+    If TET Then
+        If R < 30% Then BR * (1 + (0.1 x FS)) Else
+            If R >= 30% And R < 90% Then RT + (BR * (1 + (.02 * (FS - FST)))) Else
+                If R >= 90% Then 90%
+R = Rate
+BR = BaseRate
+FS = Failstack
+RT = Rate Surpassing Threshold
+FST = Last Failstack Where Rate < Threshold
+"""
